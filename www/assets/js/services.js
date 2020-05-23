00fc4696a5743181c4f2ca9a5dd574442377c068
ASUS <zohreh.rezayat@mail.polimi.it>
2020-05-24 01:53:03 +0200

    debug

--- www/assets/js/services.js
+++ www/assets/js/services.js
@@ -1,5 +1,6 @@
+/*
 var servicesList= document.querySelector("ul");
-fetch('https://hypergroup.herokuapp.com/v1/services')
+fetch('https://hypergroup.herokuapp.com/v1/services',{ mode: 'no-cors'})
 .then(function(response){
     var a=response.json();
     window.alert(a);
@@ -14,4 +15,19 @@ fetch('https://hypergroup.herokuapp.com/v1/services')
   }
   })
 
-
+*/
+var servicesList= document.querySelector("ul");
+fetch('https://hypergroup.herokuapp.com/v1/services')
+.then(function(response){
+    var a=response.json();
+    return a;
+}).then(function(json){
+    for(var i=0; i<json.length; i++){
+        var listItem = document.createElement("li");
+        listItem.className = "list-group list-group-flush";
+        let {id,created_at,updated_at,name,description,photo,enabled,lastname,email} = json[i];
+                window.alert(name);
+        listItem.innerHTML='<div class="card mb-3" ><div class="row no-gutters"><div class="col-md-2"><img src="..." class="card-img" alt="..."></div><div class="col-md-10"><div class="card-body"><h5 class="card-title">${name}</h5><p class="card-text">${description}</p></div></div></div></div>';
+        servicesList.appendChild(listItem);
+  }
+  })
