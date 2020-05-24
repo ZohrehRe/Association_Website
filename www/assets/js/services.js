a2fdb8113feb92b2326047475743d56d7c7d6d70
ASUS <zohreh.rezayat@mail.polimi.it>
2020-05-24 10:49:03 +0200

    debug3

--- www/assets/js/services.js
+++ www/assets/js/services.js
@@ -27,7 +27,7 @@ fetch('https://hypergroup.herokuapp.com/v1/services')
         listItem.className = "list-group list-group-flush";
         let {id,created_at,updated_at,name,description,photo,enabled,lastname,email} = json[i];
                 window.alert(name);
-        listItem.innerHTML='<div class="card mb-3" ><div class="row no-gutters"><div class="col-md-2"><img src="..." class="card-img" alt="..."></div><div class="col-md-10"><div class="card-body"><h5 class="card-title">${name}</h5><p class="card-text">${description}</p></div></div></div></div>';
+        listItem.innerHTML='<div class="card mb-3" ><div class="row no-gutters"><div class="col-md-2"><img src="..." class="card-img" alt="..."></div><div class="col-md-10"><div class="card-body"><h5 class="card-title">'+name+'</h5><p class="card-text">'+description+'</p></div></div></div></div>';
         servicesList.appendChild(listItem);
   }
   })
