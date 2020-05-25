07bb7f8e3f98d046c727fde36a87d95e27a945ea
ASUS <zohreh.rezayat@mail.polimi.it>
2020-05-25 20:41:27 +0200

    service page get

--- www/assets/js/service.js
+++ www/assets/js/service.js
@@ -1,2 +1,22 @@
 let id= document.location.search.replace(/^.*?\=/,'');
 window.alert(id);
+var url='https://hypergroup.herokuapp.com/v1/services/'+id
+var bcList= document.getElementById('breadcrumb');
+fetch(url)
+.then(function(response){
+    var a=response.json();
+    return a;
+}).then(function(json){
+    for(var i=0; i<json.length; i++){
+        var item = document.createElement("li");
+        item.className = "breadcrumb-item active";
+        cartDiv.id = "bcID"
+        document.getElementById("bcID").setAttribute('aria-current',"page");
+        let {id,created_at,updated_at,name,description,photo,enabled} = json[i];
+        item.innerHTML=name;
+        bdList.appendChild(item);
+        document.getElementById("img").setAttribute('src',"photo");
+        document.getElementById("name").innerHTML=name;
+        document.getElementById("desc").innerHTML=description;
+  }
+  })
