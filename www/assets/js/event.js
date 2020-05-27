ec85abbf4966486eadeff52588d8b5c8908dd3f4
ASUS <zohreh.rezayat@mail.polimi.it>
2020-05-27 15:47:36 +0200

    events services

--- www/assets/js/event.js
+++ www/assets/js/event.js
@@ -36,3 +36,20 @@ fetch('https://hypergroup.herokuapp.com/v1/events/'+id+'/related_persons')
         list.appendChild(item);
   }
   })
+
+
+var list2= document.getElementById('services');
+fetch('https://hypergroup.herokuapp.com/v1/events/'+id+'/related_services')
+.then(function(response){
+    var a=response.json();
+    return a;
+}).then(function(json){
+    for(var i=0; i<json.length; i++){
+        var item = document.createElement("a");
+        item.className = "list-group-item list-group-item-action";
+        let {id,created_at,updated_at,name,description,photo,enabled} = json[i];
+        item.setAttribute('href',"service.html?id="+id);
+        item.innerHTML=name;
+        list2.appendChild(item);
+  }
+  })
