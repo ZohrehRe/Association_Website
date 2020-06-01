3a23e0b053888d4479aad55f130a228db1bafcf1
ASUS <zohreh.rezayat@mail.polimi.it>
2020-06-01 02:08:28 +0200

    service-event breadcrumb

--- www/assets/js/service-event.js
+++ www/assets/js/service-event.js
@@ -10,41 +10,40 @@ fetch('https://hypergroup.herokuapp.com/v1/services/'+id+'/related_events')
 }).then(function(json){
 
     //get name
-var serviceName;
 fetch('https://hypergroup.herokuapp.com/v1/services/'+id)
 .then(function(response){
     return response.json();
 }).then(function(json){
         let {id,created_at,updated_at,name,description,photo,enabled} = json[0];
-        serviceName=name;
+        //change title
+        var ti= document.getElementById("title");
+        ti.innerHTML="Events Related To " + name;
+        var te= document.getElementById("text");
+        te.innerHTML="Here you can find all events that are related to " + name;
+
+        //change breadcrumps
+        var bc= document.getElementById("bc");
+
+        var li0 = document.createElement("li");
+        li0.className = "breadcrumb-item";
+        li0.setAttribute('aria-current',"page");
+        li0.innerHTML='<a href="services.html">Services</a>';
+        bc.appendChild(li0);
+
+        var li = document.createElement("li");
+        li.className = "breadcrumb-item";
+        li.setAttribute('aria-current',"page");
+        li.innerHTML='<a href="service.html?id='+id+'">'+name+'</a>';
+        bc.appendChild(li);
+
+        var li2 = document.createElement("li");
+            li2.className = "breadcrumb-item active";
+            li2.setAttribute('aria-current',"page");
+            li2.innerHTML="events";
+            bc.appendChild(li2);
   })
 
-    //change title
-    var ti= document.getElementById("title");
-    ti.innerHTML="Events Related To " + serviceName;
-    var te= document.getElementById("text");
-    te.innerHTML="Here you can find all events that are related to " + serviceName;
 
-    //change breadcrumps
-    var bc= document.getElementById("bc");
-
-    var li0 = document.createElement("li");
-    li0.className = "breadcrumb-item";
-    li0.setAttribute('aria-current',"page");
-    li0.innerHTML='<a href="services.html">Services</a>';
-    bc.appendChild(li0);
-
-    var li = document.createElement("li");
-    li.className = "breadcrumb-item";
-    li.setAttribute('aria-current',"page");
-    li.innerHTML='<a href="service.html?id='+id+'">'+serviceName+'</a>';
-    bc.appendChild(li);
-
-    var li2 = document.createElement("li");
-        li2.className = "breadcrumb-item active";
-        li2.setAttribute('aria-current',"page");
-        li2.innerHTML="events";
-        bc.appendChild(li2);
 
     for(var i=0; i<json.length; i++){
         var listItem = document.createElement("li");
